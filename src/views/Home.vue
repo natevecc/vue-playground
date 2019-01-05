5a2610bced079a12f7627179d26e61f50ff3ed2e
Nathan Vecchiarelli <natevecc@gmail.com>
2019-01-05 14:05:08 -0500

    initial rename

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <VuePlayground msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import VuePlayground from '@/components/VuePlayground.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    VuePlayground,
   },
 };
 </script>
